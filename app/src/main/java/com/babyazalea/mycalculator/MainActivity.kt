0506abef37475ccd45f3e0342091ba4a18483b4c
babyazalea <usun12@hotmail.com>
2020-12-16 15:35:00 +0900

    added function for other operation & remove-dot-zero & modified display result

--- app/src/main/java/com/babyazalea/mycalculator/MainActivity.kt
+++ app/src/main/java/com/babyazalea/mycalculator/MainActivity.kt
@@ -56,12 +56,52 @@ class MainActivity : AppCompatActivity() {
                         one = prefix + one
                     }
 
-                    tvInput.text =(one.toDouble() - two.toDouble()).toString()
+                    tvInput.text =removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text =removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                } else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text =removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                } else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text =removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
                 }
             }catch (error: ArithmeticException){
                 error.printStackTrace()
             }
         }
+    }
+
+    private fun removeZeroAfterDot(result: String) : String{
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+        return value
     }
 
     fun onOperator(view: View) {
